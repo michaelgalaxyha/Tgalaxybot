d1921f2e7c354e9213daa4c5fc50f5a8df94f55e
michaelgalaxyha <ha.hamed48@yahoo.com>
2016-10-17 03:00:46 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,12 +221,13 @@ function create_config( )
     "get",
     "broadcast",
     "download_media",
+    "badword",		
     "invite",
     "all",
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {157714280,73226298,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
